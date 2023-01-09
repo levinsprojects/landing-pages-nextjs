99d630afe040172dcba292d5fefb28b977ead94e
_levins <developer.levins@gmail.com>
2023-01-09 22:38:25 +0530

    feat: /about-us/social-responsibilities page added

--- sections/StickyTabsSection.tsx
+++ sections/StickyTabsSection.tsx
@@ -1,6 +1,14 @@
 import Link from "next/link";
+import { useRouter } from 'next/router';
 
 const StickyTabsSection = ({ tabs }: { tabs: { label: string, url:string, disabled?:boolean }[]}) => {
+
+    const router = useRouter();
+
+    const isSelectedUrl = (tab:any)=>{
+        return router.pathname === tab.url;
+    }
+
     return (
         <section className="bg-[#f9f2f7] hidden lg:block border-b sticky z-30 top-[85px] border-brand-100">
             <ul className="container lg:grid lg:grid-cols-2 text-center text-sm">
@@ -8,7 +16,7 @@ const StickyTabsSection = ({ tabs }: { tabs: { label: string, url:string, disabl
                     tabs.map((tab,ind)=>{
                         return !tab.disabled && <li key={ind} className={`border-l border-b ${(tabs.length - 1 === ind) ? 'border-r' : ''} md:border-b-0 border-brand-100/50`}>
                             <Link
-                                className="py-3 md:py-5 block text-slate-500 hover:text-slate-800 hover:font-bold font-normal"
+                                className={`py-3 md:py-5 block text-slate-500 hover:text-slate-800 ${isSelectedUrl(tab) ? "font-bold" :"hover:font-bold font-normal"}`}
                                 href={tab.url}
                             >{tab.label}</Link>
                         </li>
